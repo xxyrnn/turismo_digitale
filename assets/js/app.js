bb980ee26358c28913b3973ec28f41355eb6be7b
xxyrnn <xyrnproject@gmail.com>
2025-04-24 11:00:04 +0200

    Changed navbar

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open");
+    navMenu.style.display = hamburger.classList.contains("open") ? "block" : "none";
+});
