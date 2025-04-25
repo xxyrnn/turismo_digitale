88668e90549cc1fad39507536da86a22278f7d2a
xxyrnn <xyrnproject@gmail.com>
2025-04-25 16:00:22 +0200

    Changed navbar animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,5 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("nav ul");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("open");
-    navMenu.style.width = hamburger.classList.contains("open") ? "100%" : "0";
-    // navMenu.style.right = hamburger.classList.contains("open") ? "0" : "-100%";
+    navMenu.style.height = hamburger.classList.contains("open") ? "calc(100vh - 70px)" : "0";
 });
