e468afe5bbd942e28446c72024d314fbe6770b88
xxyrnn <xyrnproject@gmail.com>
2025-04-25 20:11:34 +0200

    Finished navbar and started main

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // hamburger
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("nav ul");
+const navMenu = document.querySelector(".nav-items");
+// const navMenu = document.querySelector("nav ul");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("open");
     navMenu.style.height = hamburger.classList.contains("open") ? "calc(100vh - 70px)" : "0";
