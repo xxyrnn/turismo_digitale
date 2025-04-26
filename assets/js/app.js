7dffd8c0afff088734bdaa2481d79a8227306f0e
xxyrnn <xyrnproject@gmail.com>
2025-04-27 01:15:37 +0200

    Started making slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,23 @@
 // hamburger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-items");
-// const navMenu = document.querySelector("nav ul");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("open");
     navMenu.style.height = hamburger.classList.contains("open") ? "calc(100vh - 70px)" : "0";
+});
+
+// slideshow
+const nextSlide = document.querySelector("#next");
+const prevSlide = document.querySelector("#prev");
+nextSlide.addEventListener("click", () => {
+    const currentSlide = document.querySelector(".slides img.active");
+    const next = currentSlide.nextElementSibling || document.querySelector(".slides img:first-child");
+    currentSlide.classList.remove("active");
+    next.classList.add("active");
+});
+prevSlide.addEventListener("click", () => {
+    const currentSlide = document.querySelector(".slides.active");
+    const prev = currentSlide.previousElementSibling || document.querySelector(".slides img:last-child");
+    currentSlide.classList.remove("active");
+    prev.classList.add("active");
 });
