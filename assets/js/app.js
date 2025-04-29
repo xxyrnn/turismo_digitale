f20534ea280f48c658f0281c714e46a0f70fb466
xxyrnn <xyrnproject@gmail.com>
2025-04-29 12:58:45 +0200

    Added dots

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,23 @@ hamburger.addEventListener("click", () => {
 // slideshow
 const nextSlide = document.querySelector("#next");
 nextSlide.addEventListener("click", () => {
-    const currentSlide = document.querySelector(".slides img.active");
-    const next = currentSlide.nextElementSibling || document.querySelector(".slides img:first-child");
+    const currentSlide = document.querySelector(".slides .active");
+    const currentDot = document.querySelector(".dots-container .active");
+    const nextSlide = currentSlide.nextElementSibling || document.querySelector(".slides img:first-child");
+    const nextDot = currentDot.nextElementSibling || document.querySelector(".dot:first-child");
+    currentDot.classList.remove("active");
     currentSlide.classList.remove("active");
-    next.classList.add("active");
+    nextSlide.classList.add("active");
+    nextDot.classList.add("active");
 });
 const prevSlide = document.querySelector("#prev");
 prevSlide.addEventListener("click", () => {
-    const currentSlide = document.querySelector(".slides.active");
-    const prev = currentSlide.previousElementSibling || document.querySelector(".slides img:last-child");
+    const currentSlide = document.querySelector(".slides .active");
+    const currentDot = document.querySelector(".dots-container .active");
+    const prevSlide = currentSlide.previousElementSibling || document.querySelector(".slides img:last-child");
+    const prevDot = currentDot.previousElementSibling || document.querySelector(".dot:last-child");
     currentSlide.classList.remove("active");
-    prev.classList.add("active");
+    currentDot.classList.remove("active");
+    prevSlide.classList.add("active");
+    prevDot.classList.add("active");
 });
