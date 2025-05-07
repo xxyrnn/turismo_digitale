3aadcf9a0e0bedab4debe5e6eb3597d21d78365d
xxyrnn <xyrnproject@gmail.com>
2025-05-08 01:14:06 +0200

    Nearly finished itinerary.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// hamburger
+/* hamburger */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-items");
 hamburger.addEventListener("click", () => {
@@ -6,7 +6,7 @@ hamburger.addEventListener("click", () => {
     navMenu.style.height = hamburger.classList.contains("open") ? "calc(100vh - 70px)" : "0";
 });
 
-// dropdown menu
+/* dropdown menu */
 const villagesBtn = document.getElementById("villages");
 const dropdown = document.querySelector(".dropdown");
 const villagesSpan = document.querySelector("#villages span");
@@ -23,7 +23,7 @@ document.addEventListener("click", (event) => {
     }
 });
 
-// slideshow
+/* slideshow */
 // next slide
 function slideToNext() {
     const currentSlide = document.querySelector(".slides .active");
@@ -35,12 +35,8 @@ function slideToNext() {
     nextSlide.classList.add("active");
     nextDot.classList.add("active");
 }
-
-const next = document.querySelector(".slideshow-controls #next");
-next.addEventListener("click", slideToNext);
-// prev slide
-const prev = document.querySelector(".slideshow-controls #prev");
-prev.addEventListener("click", () => {
+// previous slide
+function slideToPrev() {
     const currentSlide = document.querySelector(".slides .active");
     const currentDot = document.querySelector(".dots-container .active");
     const prevSlide = currentSlide.previousElementSibling || document.querySelector(".slides .slide:last-child");
@@ -49,29 +45,48 @@ prev.addEventListener("click", () => {
     currentDot.classList.remove("active");
     prevSlide.classList.add("active");
     prevDot.classList.add("active");
-});
-// auto slide
-let slideInterval = setInterval(slideToNext, 5000);
-// stop auto slide on hover
-const slideshow = document.querySelector(".slideshow");
-slideshow.addEventListener("mouseover", () => {
-    clearInterval(slideInterval);
-});
-// start auto slide on mouseout
-slideshow.addEventListener("mouseout", () => {
-    slideInterval = setInterval(slideToNext, 5000);
-});
-// dots
-const dots = document.querySelectorAll(".dot");
-dots.forEach((dot) => {
-    dot.addEventListener("click", () => {
-        const currentSlide = document.querySelector(".slides .active");
-        const currentDot = document.querySelector(".dots-container .active");
-        const index = Array.from(dots).indexOf(dot);
-        const targetSlide = document.querySelectorAll(".slides .slide")[index];
-        currentSlide.classList.remove("active");
-        currentDot.classList.remove("active");
-        targetSlide.classList.add("active");
-        dot.classList.add("active");
+}
+
+if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+    // auto slide
+    let slideInterval = setInterval(slideToNext, 5000);
+    // restart auto slide on mouseout
+    const slideshow = document.querySelector(".slideshow");
+    slideshow.addEventListener("mouseout", () => {
+        slideInterval = setInterval(slideToNext, 5000);
+    });
+    // dots
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot) => {
+        dot.addEventListener("click", () => {
+            const currentSlide = document.querySelector(".slides .active");
+            const currentDot = document.querySelector(".dots-container .active");
+            const index = Array.from(dots).indexOf(dot);
+            const targetSlide = document.querySelectorAll(".slides .slide")[index];
+            currentSlide.classList.remove("active");
+            currentDot.classList.remove("active");
+            targetSlide.classList.add("active");
+            dot.classList.add("active");
+        });
+    });
+}
+
+/* form */
+const inputFields = document.querySelectorAll("form input:not(#kids)");
+const errorMessage = document.getElementById("errorMsg");
+let errorInputs = new Set();
+inputFields.forEach((input) => {
+    input.addEventListener("blur", () => {
+        if (input.value.trim() === "") {
+            input.previousElementSibling.classList.add("error");
+            errorMessage.style.opacity = "1";
+            errorInputs.add(input);
+        } else {
+            errorInputs.delete(input);
+            input.previousElementSibling.classList.remove("error");
+
+            if (errorInputs.size === 0)
+                errorMessage.style.opacity = "0";
+        }
     });
 });
